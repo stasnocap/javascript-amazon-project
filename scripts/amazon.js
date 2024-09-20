712c579bd8520cd9a7746cb4b973bade7d59a799
Стас <stasnocap@gmail.com>
2024-09-20 15:10:05 +0300

    make quantity selector works

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach(product => {
       </div>
     
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -62,13 +62,15 @@ document
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
       
+      const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+      
       const exists = cart.find(x => x.productId === productId);
       if (exists) {
-        exists.quantity++;
+        exists.quantity += quantity;
       } else {
         cart.push({
           productId,
-          quantity: 1
+          quantity: quantity
         });
       }
       
