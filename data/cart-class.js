b777e8634c49024e72881c6632cc2e72e224a3be
Стас <stasnocap@gmail.com>
2024-09-23 12:20:13 +0300

    create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,77 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+  
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+  
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems =
+        [
+          {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+          },
+          {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: '2'
+          }
+        ];
+    }
+  }
+
+  addToCart(productId) {
+    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+    const exists = this.cartItems.find(x => x.productId === productId);
+    if (exists) {
+      exists.quantity += quantity;
+    } else {
+      this.cartItems.push({
+        productId,
+        quantity,
+        deliveryOptionId: '1'
+      });
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const index = this.cartItems.findIndex(x => x.productId === productId);
+
+    if (index === -1) {
+      return;
+    }
+
+    this.cartItems.splice(index, 1);
+
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    const cartItem = this.cartItems.find(x => x.productId === productId);
+
+    cartItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+}
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 ﻿import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+import "../data/cart-class.js";
 
 renderOrderSummary();
 renderPaymentSummary();
